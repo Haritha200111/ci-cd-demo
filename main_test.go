ed905d62e6e2d8287698b365e2f9f12526ead84d
Haritha <haritha.ravichandran@multicorewareinc.com>
2025-07-23 21:31:07 +0530

    Negative test case check

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSendMessage(t *testing.T) {
-	expected := "Hello, CI/CD!"
+	expected := "Hello, CI/CD!111"
 	result := SendMessage()
 	if result != expected {
 		t.Errorf("Expected %s but got %s", expected, result)
